0deed02c0d19814adb473e31a2f01b644b9d4525
yuman211 <yukinuma0211@gmail.com>
2022-05-18 00:25:37 +0900

    step8まで

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -18,17 +18,75 @@ class ReservationController extends Controller
             $postData = $request->all();
             $sortedData = $reservation->sortReservation($postData);
 
-            if($sortedData->isEmpty()){
+            if ($sortedData->isEmpty()) {
                 $sortedData = '予約がありません';
             }
 
             return $sortedData;
-
-
         } catch (Exception $e) {
             Log::emergency($e->getMessage());
             return $e;
         }
     }
 
+    public function registerReservation(Reservation $reservation, Request $request)
+    {
+        try {
+            //スタジオ、日にち、バンドID、予約開始時間、予約終了時間がとんできたとする。
+            $postData = $request->only(['studio', 'date', 'band_id', 'start_time', 'end_time']);
+            //スタジオ、日にちで絞ったデータを取得。
+            $searchedData = $reservation->searchByStudioAndDate($postData);
+            $check_result = true;
+            //絞ったデータに値があればその配列をループで一つずつ重複チェックする。
+            if (isset($searchedData)) {
+
+                foreach ($searchedData as $each) {
+                    $start_time = $each['start_time'];
+                    $end_time = $each['end_time'];
+
+                    if ($postData['start_time'] <= $end_time && $postData['end_time'] >= $start_time) {
+                        // Log::info('重複しています');
+                        $check_result = false;
+                    } else {
+                        // Log::info('予約できます');
+                        $check_result = true;
+                    }
+                }
+            }
+            //予約ができる状況であれば登録。
+            if ($check_result) {
+                $reservation->insertReservation($postData);
+                return '予約できました';
+            } else {
+                return '予約できませんでした';
+            }
+        } catch (Exception $e) {
+            Log::emergency($e->getMessage());
+            return $e;
+        }
+    }
 }
+
+// //スタジオ、日にち、バンドID、予約開始時間、予約終了時間がとんできたとする。
+// $postData = $request->only(['studio', 'date', 'band_id', 'start_time', 'end_time']);
+// //スタジオ、日にちで絞ったデータを取得。
+// $searchedData = $reservation->searchByStudioAndDate($postData);
+// //予約の配列をループで一つずつ重複チェックする。
+
+// foreach ($searchedData as $each) {
+//     $start_time = $each['start_time'];
+//     $end_time = $each['end_time'];
+
+//     if ($postData['start_time'] <= $end_time && $postData['end_time'] >= $start_time) {
+//         Log::info('重複しています');
+//         abort(403);
+//     } else {
+//         Log::info('予約できます');
+//         $check_result = true;
+//     }
+// }
+
+// //重複チェックが全てOKであれば
+// if ($check_result) {
+//     $reservation->insertReservation($postData);
+// }
---
+++ tests/Feature/ReservationTest.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Reservation;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+
+class ReservationTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * @test
+     */
+    public function 時間とスタジオが重複したら予約ができない()
+    {
+
+        //準備
+        Reservation::insert([
+            [
+                'studio' => 'A',
+                'band_id' => 2,
+                'date' => "2022-5-17",
+                'start_time' => "9:00:00",
+                'end_time' => "12:00:00",
+            ],
+            [
+                'studio' => 'A',
+                'band_id' => 2,
+                'date' => "2022-5-17",
+                'start_time' => "15:00:00",
+                'end_time' => "19:00:00",
+            ],
+        ]);
+
+        $postData = [
+            'studio' => 'A',
+            'band_id' => 2,
+            'date' => "2022-5-17",
+            'start_time' => "10:00:00",
+            'end_time' => "16:00:00",
+        ];
+
+        $this->post('api/reservation/register', $postData)
+        ->assertOK();
+
+        $this->assertCount(2, Reservation::all());
+        $this->assertDatabaseMissing('reservations',$postData);
+    }
+
+    /**
+     * @test
+     */
+    public function 同じスタジオだが、予約時間が重複していないので予約できる()
+    {
+
+        //準備
+        Reservation::insert([
+            [
+                'studio' => 'A',
+                'band_id' => 2,
+                'date' => "2022-5-17",
+                'start_time' => "9:00:00",
+                'end_time' => "12:00:00",
+            ],
+            [
+                'studio' => 'A',
+                'band_id' => 2,
+                'date' => "2022-5-17",
+                'start_time' => "15:00:00",
+                'end_time' => "19:00:00",
+            ],
+        ]);
+
+        $postData = [
+            'studio' => 'A',
+            'band_id' => 2,
+            'date' => "2022-5-17",
+            'start_time' => "20:00:00",
+            'end_time' => "21:00:00",
+        ];
+
+        $this->post('api/reservation/register', $postData)
+        ->assertOK();
+
+        $this->assertCount(3, Reservation::all());
+        $this->assertDatabaseHas('reservations',$postData);
+    }
+
+    /**
+     * @test
+     */
+    public function 予約時間が重複しているがスタジオが違うので予約できる()
+    {
+
+        //準備
+        Reservation::insert([
+            [
+                'studio' => 'A',
+                'band_id' => 2,
+                'date' => "2022-5-17",
+                'start_time' => "9:00:00",
+                'end_time' => "12:00:00",
+            ],
+            [
+                'studio' => 'A',
+                'band_id' => 2,
+                'date' => "2022-5-17",
+                'start_time' => "15:00:00",
+                'end_time' => "19:00:00",
+            ],
+        ]);
+
+        $postData = [
+            'studio' => 'C',
+            'band_id' => 2,
+            'date' => "2022-5-17",
+            'start_time' => "10:00:00",
+            'end_time' => "16:00:00",
+        ];
+
+        $this->post('api/reservation/register', $postData)
+        ->assertOK();
+
+        $this->assertCount(3, Reservation::all());
+        $this->assertDatabaseHas('reservations', $postData);
+    }
+}
